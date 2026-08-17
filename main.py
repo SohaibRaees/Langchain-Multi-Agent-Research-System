e0e0a1cadd6f990e810a4fdeda4c69a614582b9c
Sohaib <sohaibk1145@gmail.com>
2026-08-17 15:01:09 -0700

    agents working and tested

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from src.tools.tools import web_search,scrape_url
+# from src.tools.tools import web_search,scrape_url
 
-result = web_search.invoke("latest advancements in AI")
+# result = web_search.invoke("latest advancements in AI")
 
 
-print(result)
+# print(result)
+
+
+from src.pipeline.pipeline import run_research_pipeline
+
+
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
